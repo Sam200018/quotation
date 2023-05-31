3c191840d374a6ed8c87659d2560b571f11ad0fc
Samuel Bautista <samuelbaubas@gmail.com>
2023-05-31 12:33:01 -0600

    Se agregaron mas strings a strings.dart para evitar el harcode

--- lib/ui/values/strings.dart
+++ lib/ui/values/strings.dart
@@ -1,3 +1,12 @@
 // Home page
 const String beginButtonLabel = "Comenzar";
 const String protectMessage= "Protege lo que mas quieres con un solo click";
+
+// Car data
+const String carDataTitle= "Datos del automovil";
+
+//Utils
+const String nextButtonLabel= "Siguiente";
+const String quoteButtonLabel= "Cotizar";
+const String editButtonLabel= "Editar";
+
