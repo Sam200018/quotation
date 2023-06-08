ce36e86e612d99163e188ed2d59d53bc604f602c
Samuel Bautista <samuelbaubas@gmail.com>
2023-06-07 21:47:52 -0600

    Actualizacion de repo y datasource

--- lib/domain/repository/CarsRepository.dart
+++ lib/domain/repository/CarsRepository.dart
@@ -2,6 +2,6 @@ import 'package:CotizApp/model/car.dart';
 
 abstract class CarsRepository{
   Future<List<CarModel>> getCarsDataByMake(String make);
-  Future<List<CarModel>> getCarDataByModelAndYear(String make, int year);
+  Future<List<CarModel>> getCarDataByMakeAndYear(String make, String year);
   Future<CarModel>  getCarDataByModelAndMakeAndYear(String make, String model, int year);
 }
