48b195320d95d648527ed60a453e76a687fe6099
Samuel Bautista <samuelbaubas@gmail.com>
2023-06-12 22:52:13 -0600

    Se agregaron mas pantallas

--- lib/ui/routes.dart
+++ lib/ui/routes.dart
@@ -1,23 +1,29 @@
-
-//import 'dart:js';
-
 import 'package:CotizApp/ui/pages/home_page.dart';
 import 'package:CotizApp/ui/pages/car_data_page.dart';
+import 'package:CotizApp/ui/pages/personal_data_page.dart';
+import 'package:CotizApp/ui/pages/result_page.dart';
 import 'package:CotizApp/ui/pages/state_car.dart';
-import 'package:flutter/foundation.dart';
 import 'package:go_router/go_router.dart';
+
+import 'pages/insurance_data_page.dart';
 
 class AppRouter {
   static String home = "/";
   static String carData = "/carData";
   static String stateCar = "/stateCar";
+  static String persData = "/personalData";
+  static String insuData = "/insuranceData";
+  static String result = "/result";
 
   GoRouter router = GoRouter(
     initialLocation: home,
     routes: [
       GoRoute(path: home, builder: (context, state) => const HomePage()),
       GoRoute(path: carData,builder: (context, state) => const CarDataPage()),
-      GoRoute(path: stateCar,builder: (context, state) => const StateCarPage())
+      GoRoute(path: stateCar,builder: (context, state) => const StateCarPage()),
+      GoRoute(path: persData,builder: (context, state) => const PersonalDataPage()),
+      GoRoute(path: insuData,builder: (context, state) => const InsuranceDataPage()),
+      GoRoute(path: result,builder: (context,state)=> const ResultPage())
     ],
   );
 }
